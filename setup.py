b30abad3cd8e6c3f5fe6fc8be0b6b24e95ed4d29
harshita-agr <42787512+harshita-agr@users.noreply.github.com>
2020-01-27 02:06:50 -1200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+  name = 'topsis_code',         
+  packages = ['topsis_code'],   
+  version = '0.1',      
+  license='MIT',        
+  description = 'code on topsis',  
+  author = 'YOUR NAME',                   # Type in your name
+  author_email = 'harshitaagrawal1998@gmail.com',      
+  url = 'https://github.com/harshita-agr/topsis_code',   
+  download_url = 'https://github.com/harshita-agr/topsis_code/archive/v_01.tar.gz',    
+  keywords = ['TOPSIS'],
+  install_requires=[                     
+          'pandas', 
+           'numpy', 
+           'sys' ,
+           'argparse'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',       
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
